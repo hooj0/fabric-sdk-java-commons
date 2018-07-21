efc02b5f50116723b1962348d71bb73fe3177566
hoojo <hoojo@qq.com>
2018-07-21 23:28:08 +0800

    :sparkles: :bento: Add fabric config class

---
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/config/FabricConfiguration.java
@@ -0,0 +1,16 @@
+package io.github.hooj0.fabric.sdk.commons.config;
+
+/**
+ * fabric config
+ * @author hoojo
+ * @createDate 2018年7月21日 下午11:17:03
+ * @file FabricConfiguration.java
+ * @package io.github.hooj0.fabric.sdk.commons.config
+ * @project fabric-sdk-commons
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class FabricConfiguration {
+
+}
