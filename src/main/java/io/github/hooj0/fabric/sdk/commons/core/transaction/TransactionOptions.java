5d9a49d37cb9d61b53bb12cc7a92c145d19c8c17
hoojo <hoojo@qq.com>
2018-07-21 23:29:34 +0800

    :sparkles: :bento: Add fabric transaction option

---
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/core/transaction/TransactionOptions.java
@@ -0,0 +1,16 @@
+package io.github.hooj0.fabric.sdk.commons.core.transaction;
+
+/**
+ * 
+ * @author hoojo
+ * @createDate 2018年7月21日 下午11:25:04
+ * @file TransactionOptions.java
+ * @package io.github.hooj0.fabric.sdk.commons.core.transaction
+ * @project fabric-sdk-commons
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class TransactionOptions {
+
+}
