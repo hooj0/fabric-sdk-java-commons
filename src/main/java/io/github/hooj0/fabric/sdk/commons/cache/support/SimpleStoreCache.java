14c3aebfb2b31ba5de677c71bd286a228f9d40f8
hoojo <hoojo@qq.com>
2018-07-22 16:21:20 +0800

    :sparkles: :bento: Add fabric store cache `simple` implements

---
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/cache/support/SimpleStoreCache.java
@@ -0,0 +1,33 @@
+package io.github.hooj0.fabric.sdk.commons.cache.support;
+
+import io.github.hooj0.fabric.sdk.commons.cache.AbstractStoreCache;
+import io.github.hooj0.fabric.sdk.commons.cache.CacheKeyPrefix;
+import io.github.hooj0.fabric.sdk.commons.store.FabricKeyValueStore;
+
+/**
+ * simple store cache support
+ * @author hoojo
+ * @createDate 2018年7月22日 下午2:49:05
+ * @file SimpleStoreCache.java
+ * @package io.github.hooj0.fabric.sdk.commons.cache.support
+ * @project fabric-sdk-commons
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class SimpleStoreCache extends AbstractStoreCache<String> {
+
+	public SimpleStoreCache(CacheKeyPrefix keyPrefix, FabricKeyValueStore keyValueStore) {
+		super(keyPrefix, keyValueStore);
+	}
+
+	@Override
+	public String serialize(String store) {
+		return store;
+	}
+
+	@Override
+	public String deserialize(String value) {
+		return value;
+	}
+}
