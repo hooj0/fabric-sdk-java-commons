8d3c133169dd066e86b6e5233c05bcaadc02c901
hoojo <hoojo@qq.com>
2018-07-22 16:36:48 +0800

    :sparkles: :bento: Add system commons abstract Object

--- src/main/java/io/github/hooj0/fabric/sdk/commons/AbstractObject.java
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/AbstractObject.java
@@ -8,7 +8,7 @@ import org.apache.commons.lang3.builder.ToStringStyle;
  * @author hoojo
  * @createDate 2018年6月12日 下午3:12:36
  * @file AbstractFabricObject.java
- * @package com.cnblogs.hoojo.fabric.sdk.common
+ * @package com.cnblogs.hoojo.fabric.sdk.commons
  * @project fabric-sdk-examples
  * @blog http://hoojo.cnblogs.com
  * @email hoojo_@126.com
