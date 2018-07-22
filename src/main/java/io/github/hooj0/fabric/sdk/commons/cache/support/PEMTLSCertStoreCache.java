1d0a5a5ef3624cd2d3b15d717db54222e732d43c
hoojo <hoojo@qq.com>
2018-07-22 16:21:43 +0800

    :sparkles: :bento: Add fabric store cache `PEM TLS cert` implements

---
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/cache/support/PEMTLSCertStoreCache.java
@@ -0,0 +1,22 @@
+package io.github.hooj0.fabric.sdk.commons.cache.support;
+
+import io.github.hooj0.fabric.sdk.commons.cache.CacheKeyPrefix;
+import io.github.hooj0.fabric.sdk.commons.store.FabricKeyValueStore;
+
+/**
+ * PEM TLS Cert sotre cache support, 保存客户端证书   PEM tls cert 
+ * @author hoojo
+ * @createDate 2018年7月22日 下午2:08:03
+ * @file PemTLSCertStoreCache.java
+ * @package io.github.hooj0.fabric.sdk.commons.cache.support
+ * @project fabric-sdk-commons
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class PEMTLSCertStoreCache extends SimpleStoreCache {
+
+	public PEMTLSCertStoreCache(FabricKeyValueStore keyValueStore) {
+		super(CacheKeyPrefix.PEM_TLS_CERT_PREFIX, keyValueStore);
+	}
+}
