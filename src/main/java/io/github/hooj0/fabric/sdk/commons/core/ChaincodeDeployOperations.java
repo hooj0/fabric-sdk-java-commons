eae19e889029412b0b9811eb49c28377413c7cd5
hoojo <hoojo@qq.com>
2018-07-24 23:09:34 +0800

    :sparkles: :bento: Add chaincode deploy operation interface

---
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/core/ChaincodeDeployOperations.java
@@ -0,0 +1,16 @@
+package io.github.hooj0.fabric.sdk.commons.core;
+
+/**
+ * <b>function:</b>
+ * @author hoojo
+ * @createDate 2018年7月24日 下午5:46:02
+ * @file ChaincodeDeployOperations.java
+ * @package io.github.hooj0.fabric.sdk.commons.core
+ * @project fabric-sdk-commons
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public interface ChaincodeDeployOperations {
+
+}
