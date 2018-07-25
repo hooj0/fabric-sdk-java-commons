801553f491d603753a1f01a497de0b63c4e3819d
hoojo <hoojo@qq.com>
2018-07-25 18:59:46 +0800

    :sparkles::bento::recycle: Changed  chaincode transaction query execution interface support

--- src/main/java/io/github/hooj0/fabric/sdk/commons/core/execution/support/ChaincodeQueryExecutionSupport.java
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/core/execution/support/ChaincodeQueryExecutionSupport.java
@@ -1,6 +1,7 @@
 package io.github.hooj0.fabric.sdk.commons.core.execution.support;
 
 import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 import java.util.Collection;
@@ -19,12 +20,12 @@ import com.google.common.base.Strings;
 
 import io.github.hooj0.fabric.sdk.commons.FabricChaincodeQueryException;
 import io.github.hooj0.fabric.sdk.commons.core.execution.ChaincodeQueryExecution;
-import io.github.hooj0.fabric.sdk.commons.core.execution.option.Options;
-import io.github.hooj0.fabric.sdk.commons.core.execution.option.SimpleOptions;
+import io.github.hooj0.fabric.sdk.commons.core.execution.option.FuncOptions;
+import io.github.hooj0.fabric.sdk.commons.core.execution.option.QueryOptions;
 import io.github.hooj0.fabric.sdk.commons.core.execution.result.ResultSet;
 
 /**
- * <b>function:</b>
+ * chaincode transaction query execution interface support 
  * @author hoojo
  * @createDate 2018年7月24日 上午10:25:31
  * @file ChaincodeQueryExecutionSupport.java
@@ -41,62 +42,65 @@ public class ChaincodeQueryExecutionSupport extends AbstractChaincodeExecutionSu
 	}
 	
 	@Override
-	public String execute(Options options, String func) {
-		return this.execute(bindOptions(options, new SimpleOptions(func)));
+	public String execute(QueryOptions options, String func) {
+		return this.execute(options, new FuncOptions(func));
 	}
 
 	@Override
-	public String execute(Options options, String func, Object... args) {
-		return this.execute(bindOptions(options, new SimpleOptions(func, args)));
+	public String execute(QueryOptions options, String func, Object... args) {
+		return this.execute(options, new FuncOptions(func, args));
 	}
 
 	@Override
-	public String execute(Options options, String func, LinkedHashMap<String, Object> args) {
-		return this.execute(bindOptions(options, new SimpleOptions(func, args)));
+	public String execute(QueryOptions options, String func, LinkedHashMap<String, Object> args) {
+		return this.execute(options, new FuncOptions(func, args));
 	}
 
 	@Override
-	public String execute(SimpleOptions options) {
-		return this.executeFor(options).getResult();
+	public String execute(QueryOptions options, FuncOptions funcOptions) {
+		return this.executeFor(options, funcOptions).getResult();
 	}
 
 	@Override
-	public ResultSet executeFor(Options options, String func) {
-		return this.executeFor(bindOptions(options, new SimpleOptions(func)));
+	public ResultSet executeFor(QueryOptions options, String func) {
+		return this.executeFor(options, new FuncOptions(func));
 	}
 
 	@Override
-	public ResultSet executeFor(Options options, String func, Object... args) {
+	public ResultSet executeFor(QueryOptions options, String func, Object... args) {
 		
-		return this.executeFor(bindOptions(options, new SimpleOptions(func, args)));
+		return this.executeFor(options, new FuncOptions(func, args));
 	}
 
 	@Override
-	public ResultSet executeFor(Options options, String func, Map<String, Object> args) {
-		return this.executeFor(bindOptions(options, new SimpleOptions(func, args)));
+	public ResultSet executeFor(QueryOptions options, String func, Map<String, Object> args) {
+		return this.executeFor(options, new FuncOptions(func, args));
 	}
 
-	private void checkArgs(SimpleOptions options) {
-		checkArgument(!Strings.isNullOrEmpty(options.getFunc()), "func 参数为必填项");
-		checkArgument(!Objects.isNull(options.getArgs()), "args 参数为必填项");
+	private void checkArgs(QueryOptions options, FuncOptions funcOptions) {
+		checkArgument(!Strings.isNullOrEmpty(funcOptions.getFunc()), "func 参数为必填项");
+		checkArgument(!Objects.isNull(funcOptions.getArgs()), "args 参数为必填项");
+		checkNotNull(options.getClientUserContext(), "client user 参数不可忽略设置");
 	}
 	
 	@Override
-	public ResultSet executeFor(SimpleOptions options) {
+	public ResultSet executeFor(QueryOptions options, FuncOptions funcOptions) {
 		logger.info("在通道：{} 发起chaincode 查询：{}", channel.getName(), options.getChaincodeId());
 		
-		checkArgs(options);
+		checkArgs(options, funcOptions);
 		
 		Collection<ProposalResponse> responses = null;
 		String payload = null;
 		try {
+			client.setUserContext(options.getClientUserContext());
+			
 			// 构建查询请求
 			QueryByChaincodeRequest request = client.newQueryProposalRequest();
 			request.setProposalWaitTime(options.getProposalWaitTime());
 			request.setChaincodeLanguage(options.getChaincodeType());
 			request.setChaincodeID(options.getChaincodeId());
-			request.setFcn(options.getFunc());
-			request.setArgs(options.getArgs());
+			request.setFcn(funcOptions.getFunc());
+			request.setArgs(funcOptions.getArgs());
 			
 			Map<String, byte[]> transientMap = new HashMap<>();
 			transientMap.put("HyperLedgerFabric", "QueryByChaincodeRequest:JavaSDK".getBytes(UTF_8));
@@ -106,6 +110,9 @@ public class ChaincodeQueryExecutionSupport extends AbstractChaincodeExecutionSu
 				transientMap.putAll(options.getTransientData());
 			}
             request.setTransientMap(transientMap);
+            if (options.getRequestUser() != null) {
+            	request.setUserContext(options.getRequestUser());
+            }
             
             if (options.getSend2Peers() != null) { // 向指定的Peer节点发送请求
             	responses = channel.queryByChaincode(request, options.getSend2Peers());
