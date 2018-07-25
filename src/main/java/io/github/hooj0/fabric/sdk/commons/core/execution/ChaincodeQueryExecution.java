24b6f6e9eb53b688134d6a647c60fc33c906ff0b
hoojo <hoojo@qq.com>
2018-07-25 14:14:03 +0800

    :sparkles: :bento: Added chaincode transaction query execution interface

--- src/main/java/io/github/hooj0/fabric/sdk/commons/core/execution/ChaincodeQueryExecution.java
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/core/execution/ChaincodeQueryExecution.java
@@ -2,12 +2,12 @@ package io.github.hooj0.fabric.sdk.commons.core.execution;
 
 import java.util.Map;
 
-import io.github.hooj0.fabric.sdk.commons.core.execution.option.Options;
-import io.github.hooj0.fabric.sdk.commons.core.execution.option.SimpleOptions;
+import io.github.hooj0.fabric.sdk.commons.core.execution.option.FuncOptions;
+import io.github.hooj0.fabric.sdk.commons.core.execution.option.QueryOptions;
 import io.github.hooj0.fabric.sdk.commons.core.execution.result.ResultSet;
 
 /**
- * <b>function:</b>
+ * chaincode transaction query execution interface
  * @author hoojo
  * @createDate 2018年7月24日 上午10:22:55
  * @file ChaincodeQueryExecution.java
@@ -17,13 +17,13 @@ import io.github.hooj0.fabric.sdk.commons.core.execution.result.ResultSet;
  * @email hoojo_@126.com
  * @version 1.0
  */
-public interface ChaincodeQueryExecution extends ChaincodeExecution<String, Options, SimpleOptions> {
+public interface ChaincodeQueryExecution extends ChaincodeExecution<String, QueryOptions, FuncOptions> {
 
-    ResultSet executeFor(Options options, String func);
+    ResultSet executeFor(QueryOptions options, String func);
 
-    ResultSet executeFor(Options options, String func, Object... args);
+    ResultSet executeFor(QueryOptions options, String func, Object... args);
 
-    ResultSet executeFor(Options options, String func, Map<String, Object> args);
+    ResultSet executeFor(QueryOptions options, String func, Map<String, Object> args);
 
-    ResultSet executeFor(SimpleOptions options);
+    ResultSet executeFor(QueryOptions options, FuncOptions funcOptions);
 }
