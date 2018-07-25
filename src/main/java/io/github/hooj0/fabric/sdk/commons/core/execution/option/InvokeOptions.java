41dd46dde7d90afeeb6492ccfd48c728677354b7
hoojo <hoojo@qq.com>
2018-07-25 14:08:31 +0800

    :sparkles: :bento: Added chaincode transaction invoke execution options

--- src/main/java/io/github/hooj0/fabric/sdk/commons/core/execution/option/InvokeOptions.java
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/core/execution/option/InvokeOptions.java
@@ -1,14 +1,9 @@
 package io.github.hooj0.fabric.sdk.commons.core.execution.option;
 
-import java.util.Collection;
-import java.util.Map;
-
-import org.hyperledger.fabric.sdk.Channel.TransactionOptions;
-import org.hyperledger.fabric.sdk.Orderer;
 import org.hyperledger.fabric.sdk.User;
 
 /**
- * <b>function:</b>
+ * chaincode transaction invoke execution options
  * @author hoojo
  * @createDate 2018年7月24日 上午11:40:50
  * @file InvokeOptions.java
@@ -18,63 +13,16 @@ import org.hyperledger.fabric.sdk.User;
  * @email hoojo_@126.com
  * @version 1.0
  */
-public class InvokeOptions extends SimpleOptions {
+public class InvokeOptions extends TransactionsOptions {
 
-	private int transactionWaitTime;
-	
-	private User contextUser;
-	private Collection<Orderer> orderers;
-	private TransactionOptions options;
-	
-	public InvokeOptions(String func) {
-		super(func);
-	}
-	
-	public InvokeOptions(String func, Object... values) {
-		super(func, values);
+	private User requestUser;
+
+	public User getRequestUser() {
+		return requestUser;
 	}
 
-	public InvokeOptions(String func, Map<String, Object> values) {
-		super(func, values);
-	}
-
-	public User getContextUser() {
-		return contextUser;
-	}
-
-	public InvokeOptions setContextUser(User contextUser) {
-		this.contextUser = contextUser;
-		
-		return this;
-	}
-
-	public int getTransactionWaitTime() {
-		return transactionWaitTime;
-	}
-
-	public InvokeOptions setTransactionWaitTime(int transactionWaitTime) {
-		this.transactionWaitTime = transactionWaitTime;
-		
-		return this;
-	}
-
-	public Collection<Orderer> getOrderers() {
-		return orderers;
-	}
-
-	public InvokeOptions setOrderers(Collection<Orderer> orderers) {
-		this.orderers = orderers;
-		
-		return this;
-	}
-
-	public TransactionOptions getOptions() {
-		return options;
-	}
-
-	public InvokeOptions setOptions(TransactionOptions options) {
-		this.options = options;
-		
+	public InvokeOptions setRequestUser(User requestUser) {
+		this.requestUser = requestUser;
 		return this;
 	}
 }
