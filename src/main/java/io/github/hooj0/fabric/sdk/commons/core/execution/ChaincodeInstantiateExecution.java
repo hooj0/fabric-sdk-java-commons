7e925ea62fc5aa18925e6f2531dc25df28858954
hoojo <hoojo@qq.com>
2018-07-25 14:12:40 +0800

    :sparkles: :bento: Added chaincode operation deploy instantiate execution interface

--- src/main/java/io/github/hooj0/fabric/sdk/commons/core/execution/ChaincodeInstantiateExecution.java
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/core/execution/ChaincodeInstantiateExecution.java
@@ -4,12 +4,12 @@ import java.util.concurrent.CompletableFuture;
 
 import org.hyperledger.fabric.sdk.BlockEvent.TransactionEvent;
 
+import io.github.hooj0.fabric.sdk.commons.core.execution.option.FuncOptions;
 import io.github.hooj0.fabric.sdk.commons.core.execution.option.InstantiateOptions;
-import io.github.hooj0.fabric.sdk.commons.core.execution.option.Options;
 import io.github.hooj0.fabric.sdk.commons.core.execution.result.ResultSet;
 
 /**
- * <b>function:</b>
+ * chaincode operation deploy instantiate execution interface
  * @author hoojo
  * @createDate 2018年7月24日 下午4:01:45
  * @file ChaincodeInstantiateExecution.java
@@ -19,6 +19,6 @@ import io.github.hooj0.fabric.sdk.commons.core.execution.result.ResultSet;
  * @email hoojo_@126.com
  * @version 1.0
  */
-public interface ChaincodeInstantiateExecution extends ChaincodeExecution<ResultSet, Options, InstantiateOptions>, TransactionExecution<CompletableFuture<TransactionEvent>, Options, InstantiateOptions> {
+public interface ChaincodeInstantiateExecution extends ChaincodeExecution<ResultSet, InstantiateOptions, FuncOptions>, TransactionExecution<CompletableFuture<TransactionEvent>, InstantiateOptions, FuncOptions> {
 
 }
