2706ef5ffd1ace89fc246a56cd95dbb1237e787d
hoojo <hoojo@qq.com>
2018-07-25 14:13:40 +0800

    :sparkles: :bento: Added chaincode transaction invoke operation execution interface

--- src/main/java/io/github/hooj0/fabric/sdk/commons/core/execution/ChaincodeInvokeExecution.java
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/core/execution/ChaincodeInvokeExecution.java
@@ -4,12 +4,12 @@ import java.util.concurrent.CompletableFuture;
 
 import org.hyperledger.fabric.sdk.BlockEvent.TransactionEvent;
 
+import io.github.hooj0.fabric.sdk.commons.core.execution.option.FuncOptions;
 import io.github.hooj0.fabric.sdk.commons.core.execution.option.InvokeOptions;
-import io.github.hooj0.fabric.sdk.commons.core.execution.option.Options;
 import io.github.hooj0.fabric.sdk.commons.core.execution.result.ResultSet;
 
 /**
- * <b>function:</b>
+ * chaincode transaction invoke operation execution interface
  * @author hoojo
  * @createDate 2018年7月24日 上午10:22:55
  * @file ChaincodeQueryExecution.java
@@ -19,5 +19,5 @@ import io.github.hooj0.fabric.sdk.commons.core.execution.result.ResultSet;
  * @email hoojo_@126.com
  * @version 1.0
  */
-public interface ChaincodeInvokeExecution extends ChaincodeExecution<ResultSet, Options, InvokeOptions>, TransactionExecution<CompletableFuture<TransactionEvent>, Options, InvokeOptions> {
+public interface ChaincodeInvokeExecution extends ChaincodeExecution<ResultSet, InvokeOptions, FuncOptions>, TransactionExecution<CompletableFuture<TransactionEvent>, InvokeOptions, FuncOptions> {
 }
