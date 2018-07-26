378de7ad15790ea824af0883a213de5d67b9bc47
hoojo <hoojo@qq.com>
2018-07-26 19:09:23 +0800

    :sparkles: :bento: :recycle:  Changed 	keyword=""

--- git_onekey.sh
+++ git_onekey.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e
+#set -e
 #set -o pipefail
 trap "echo 'error: Script failed: see failed command above'" ERR
 
@@ -69,106 +69,6 @@ function log() {
 	esac
 }
 
-function commitCode() {
-	#echo "##########################################################"
-	#echo "#####         git commit file & comment          #########"
-	#echo "##########################################################"
-	echo
-	
-	code_status="$1"
-	file="$2"
-	comment="$3"
-	
-	log "_black" "git add $file"
-	git add "$file"
-	
-	if [ -z $comment ]; then
-		log "red" "commit file ==> $file"
-		read -p "input commit comment message: " comment
-	fi
-	
-	case $code_status in
-	    "M")  
-			log "_blue" "Changed file ==> $file"
-	    	git commit -m ":sparkles::bento::recycle: Changed ${comment}"
-	    ;;
-	    "R")  
-			log "_yellow" "Renamed file ==> $file"
-	    	git commit -m ":sparkles::bento::truck: Renamed ${comment}"
-	    ;;
-	    "A")  
-			log "_green" "Added file ==> $file"
-	    	git commit -m ":sparkles::bento: Added ${comment}"
-	    ;;
-	    "D")  
-			log "_red" "Removed file ==> $file"
-	    	git commit -m ":sparkles::bento::fire: Removed ${comment}"
-	    ;;
-	    "C")  
-			log "_sky_blue" "Added copy file ==> $file"
-	    	git commit -m ":sparkles::bento: Added ${comment}"
-	    ;;
-	    "U")  
-			log "_white" "Added copy file ==> $file"
-	    	git commit -m ":sparkles::bento::recycle: Updated ${comment}"
-	    ;;
-	    "??")  
-			log "_purple" "First init add ==> $file"
-	    	git commit -m ":sparkles::tada::bento: Init add ${comment}"
-	    ;;
-	    *)  
-	    	log "rrr" "没有匹配的状态：$code_status"
-	    ;;
-	esac
-}
-
-function findCommitFile() {
-	echo
-	#echo "##########################################################"
-	#echo "#####           find git commit files            #########"
-	#echo "##########################################################"
-	#echo
-	
-	# git status -s | awk -F ' ' '{print $1}'
-	
-	IFS_OLD=$IFS
-	IFS=$'\n'
-	file_status=$(git status -s)
-	
-	for status in $file_status; do
-		log "_line" "                                                                                                                                "
-		log "_white" "file status ==> ${status}"
-		
-		state=`echo $status | awk -F ' ' '{print $1}'`
-		file=`echo $status | awk -F ' ' '{print $2}'`
-		
-		#echo "status --> ${state}"
-		#echo "file --> ${file}"
-		readerComment $file
-		
-		commitCode $state $file $comment
-		# printf "\n"
-	done
-	
-	IFS=$IFS_OLD
-}
-
-function readerComment() {
-	#echo "##########################################################"
-	#echo "#####       read git commit file comment         #########"
-	#echo "##########################################################"
-	#echo
-	
-	file="$1"
-	
-	# echo "read file ==> $file"
-	
-	# $ grep -B 3 " \* " $file
-	comment=`egrep -m 3 " \* " $file | sed s/\*//g | sed 's/^ //g'`
-	
-	echo "comment ==> $comment"
-}
-	
 function colors() {
 	log "red" "22222222"
 	log "yellow" "22222222"
@@ -188,5 +88,176 @@ function colors() {
 	log "_black" "22222222"
 }
 
-findCommitFile
+function commitCode() {
+	echo
+	
+	code_status="$1"
+	file="$2"
+	comment="$3"
+	
+	log "_black" "git add $file"
+	git add "$file"
+	
+	if [ -z $comment ]; then
+		log "red" "commit file ==> $file"
+		git diff $file
+		read -p "input commit comment message: " comment
+	fi
+	
+	case $code_status in
+	    "M")  
+			log "_blue" "Changed file ==> $file"
+	    	git commit -m ":sparkles: :bento: :recycle: $emoji Changed ${comment}"
+	    ;;
+	    "R"|"RMD")  
+			log "_yellow" "Renamed file ==> $file"
+	    	git commit -m ":sparkles: :bento: :truck: $emoji Renamed ${comment}"
+	    ;;
+	    "A")  
+			log "_green" "Added file ==> $file"
+	    	git commit -m ":sparkles: :bento: $emoji Added ${comment}"
+	    ;;
+	    "D")  
+			log "_red" "Removed file ==> $file"
+	    	git commit -m ":sparkles: :bento: :fire: $emoji Removed ${comment}"
+	    ;;
+	    "C")  
+			log "_sky_blue" "Added copy file ==> $file"
+	    	git commit -m ":sparkles: :bento: $emoji Added ${comment}"
+	    ;;
+	    "U")  
+			log "_white" "Added copy file ==> $file"
+	    	git commit -m ":sparkles: :bento: :recycle: $emoji Updated ${comment}"
+	    ;;
+	    "??")  
+			log "_purple" "First init add ==> $file"
+	    	git commit -m ":sparkles: :tada: :bento: $emoji Init add ${comment}"
+	    ;;
+	    *)  
+	    	log "rrr" "===================================> NOT FOUND Match Status：$code_status"
+	    ;;
+	esac
+}
 
+function findCommitFiles() {
+	echo
+	# git status -s | awk -F ' ' '{print $1}'
+	
+	IFS_OLD=$IFS
+	IFS=$'\n'
+	file_status=$(git status -s)
+	
+	for status in $file_status; do
+		log "_line" "                                                                                                                                "
+		log "_white" "file status ==> ${status}"
+		
+		state=`echo $status | awk -F ' ' '{print $1}'`
+		file=`echo $status | awk -F ' ' '{print $2}'`
+		
+		if [ $state == "RM" ]; then
+			file=`echo $status | awk -F ' -> ' '{print $2}'`
+		fi
+		
+		#echo "status --> ${state}"
+		#echo "file --> ${file}"
+		fetchComment $file
+		
+		fetchCommentStatus $comment
+		commitCode $state $file $comment
+	done
+	
+	IFS=$IFS_OLD
+}
+
+function fetchComment() {
+	file="$1"
+	#echo "read file ==> $file"
+	#grep -A 3 -m 3 " \* " $file
+	
+	keyword="@changelog"
+	defaultKeyword=" \* "
+
+	#head -100 "$file"
+	#comment=`sed -n '1,100p' $file | grep -iw -m 1 "$keyword" | sed "s/$keyword//g"`
+	comment=`grep -iw -m 1 "$keyword" $file | sed "s/$keyword//g"`
+	if [ -z $comment ]; then
+		comment=`grep -i -m 1 "$defaultKeyword" $file`
+	fi
+	
+	# replace space
+	comment=`echo $comment | sed "s/$defaultKeyword//g" | sed 's/^ //g'`
+	
+	if [ -z $comment ]; then
+		generatorComment
+	fi
+	
+	echo "comment ==> $comment"
+}
+
+function generatorComment() {
+	log "red" "----------------------> ${file##*.}"
+	
+	suffix=${file##*.}
+	case $suffix in
+		"gitignore")
+			comment="configure git 'gitignore' to ignore some files"
+			emoji=":see_no_evil:"
+		;;
+		"properties"|"xml")
+			emoji=":wrench:"
+		;;
+	esac	
+}
+
+# fetch comment content status<add/remove/modify/update/rename/delete>
+function fetchCommentStatus() {
+	
+	comment="$1"
+	comment_state=`echo $comment | awk -F ' ' '{print $1}'`
+	
+	unset fetch_status
+	if [ -n $comment_state ]; then
+		case $comment_state in
+			add|Add|added|Added)
+				fetch_status="A"
+			;;
+			remove|Remove|removed|Removed)
+				fetch_status="D"
+			;;
+			modify|Modify|modified|Modified)
+				fetch_status="M"
+			;;
+			update|Update|updated|Updated)
+				fetch_status="M"
+			;;
+			rename|Rename|renamed|Renamed)
+				fetch_status="R"
+			;;
+			delete|Delete|deleted|Deleted)
+				fetch_status="D"
+			;;
+		esac
+		
+		# 截掉首单词
+		if [ $fetch_status ]; then
+			comment=`echo $comment | sed "s/^$comment_state//"`
+			state=$fetch_status
+		fi
+	fi
+	
+	# echo "comment_state==========> $comment_state"
+	# echo "state==========> $fetch_status"
+	# echo "comment==========> $comment"
+}
+
+# setup shell
+function setup() {
+	
+	findCommitFiles
+	
+	echo
+	log "_green" "Done!"
+}
+	
+
+setup
