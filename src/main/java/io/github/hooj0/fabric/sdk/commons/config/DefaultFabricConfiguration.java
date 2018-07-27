0aa1c9ea4cbe784aa231af6ccd5038d13fc172ca
hoojo <hoojo@qq.com>
2018-07-27 19:17:00 +0800

    :sparkles: :bento: :recycle:  Changed default fabric configuration support

--- src/main/java/io/github/hooj0/fabric/sdk/commons/config/DefaultFabricConfiguration.java
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/config/DefaultFabricConfiguration.java
@@ -1,11 +1,7 @@
 package io.github.hooj0.fabric.sdk.commons.config;
 
-import java.io.File;
-import java.util.Collection;
-import java.util.Properties;
-
-import io.github.hooj0.fabric.sdk.commons.domain.Organization;
-import io.github.hooj0.fabric.sdk.commons.store.FabricKeyValueStore;
+import io.github.hooj0.fabric.sdk.commons.config.support.FabricClassConfiguration;
+import io.github.hooj0.fabric.sdk.commons.config.support.FabricPropertiesConfiguration;
 
 /**
  * default fabric configuration support
@@ -18,174 +14,34 @@ import io.github.hooj0.fabric.sdk.commons.store.FabricKeyValueStore;
  * @email hoojo_@126.com
  * @version 1.0
  */
-public enum DefaultFabricConfiguration implements FabricConfiguration {
-
+public enum DefaultFabricConfiguration {
 	INSTANCE;
+
+	public FabricConfiguration getDefaultConfiguration() {
+		return this.getClassConfiguration();
+		//return this.getPropertiesConfiguration();
+	}
 	
-	@Override
-	public FabricKeyValueStore getKeyValueStore() {
-		// TODO Auto-generated method stub
-		return null;
+	public FabricPropertiesConfiguration getPropertiesConfiguration() {
+		return FabricPropertiesConfiguration.getInstance();
+	}
+	
+	public FabricClassConfiguration getClassConfiguration() {
+		return FabricClassConfiguration.getInstance();
 	}
 
-	@Override
-	public Collection<Organization> getOrganizations() {
-		// TODO Auto-generated method stub
-		return null;
+	private DefaultFabricConfiguration() {
+		this.setLocalSettings();
 	}
+	
+	private void setLocalSettings() {
+		FabricClassConfiguration config = this.getClassConfiguration();
+		
+		config.setCaLocation("aaaaaaa", "127.0.0.1");
+		config.setNetworkDomain("hoojo.cnblogs.com");
 
-	@Override
-	public Organization getOrganization(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Properties getPeerProperties(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Properties getOrdererProperties(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Properties getEventHubProperties(String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public Properties getTLSCertProperties(String type, String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isRunningFabricTLS() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public File getNetworkConfigFile() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getOrdererOrgsDomain() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int getTransactionWaitTime() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getDeployWaitTime() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public long getProposalWaitTime() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public String getFabricConfigGeneratorVersion() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getCommonConfigRootPath() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getCryptoTxConfigRootPath() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getChannelPath() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getChaincodePath() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getEndorsementPolicyFilePath() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getNetworkConfigDirFilePath() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public String getFabricConfigTxLaterURL() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isRunningAgainstFabric10() {
-		return false;
-	}
-
-	@Override
-	public String getDefaultOrgName() {
-		return "peerOrg1";
-	}
-
-	@Override
-	public String getChannelName() {
-		return "mychannel";
-	}
-
-	@Override
-	public AdminInfo getAdminInfo() {
-		return new AdminInfo() {
-			
-			@Override
-			public String getPassword() {
-				return "adminpw";
-			}
-			
-			@Override
-			public String getName() {
-				return "admin";
-			}
-		};
-	}
-
-	@Override
-	public String[] getUsers() {
-		return new String[] { "user1" };
-	}
-
-	@Override
-	public String getFabricNetworkHost() {
-		return "192.168.8.8";
+		config.settingPropertyValue(FabricConfigurationPropertyKey.NETWORK_CA_ADMIN_NAME, "admin");
+		config.settingPropertyValue(FabricConfigurationPropertyKey.NETWORK_CA_ADMIN_PASSWD, "adminpw");
+		config.settingPropertyValue(FabricConfigurationPropertyKey.NETWORK_ORGS_MEMBER_USERS, "user1");
 	}
 }
