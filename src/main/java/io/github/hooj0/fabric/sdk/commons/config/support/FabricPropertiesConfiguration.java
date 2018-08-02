8216faf5ddd127f561c10b22d1277ffe0bfb0748
hoojo <hoojo@qq.com>
2018-08-02 08:45:52 +0800

    :sparkles: :bento:  Added  properties filesystem configuration generator file

--- src/main/java/io/github/hooj0/fabric/sdk/commons/config/support/FabricPropertiesConfiguration.java
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/config/support/FabricPropertiesConfiguration.java
@@ -40,6 +40,7 @@ public final class FabricPropertiesConfiguration extends AbstractConfigurationSu
 	
 	private FabricPropertiesConfiguration() {
 		super(FabricPropertiesConfiguration.class);
+		System.out.println("---------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 		
 		InputStream stream = null;
 		try {
