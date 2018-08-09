48ae7164371f24e7392a733547285b4d37dc6cf5
hoojo <hoojo@qq.com>
2018-08-09 19:38:24 +0800

    :sparkles: :bento:  Added  peer adimn cache store support

--- src/main/java/io/github/hooj0/fabric/sdk/commons/core/creator/OrganizationUserCreatorImpl.java
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/core/creator/OrganizationUserCreatorImpl.java
@@ -2,25 +2,22 @@ package io.github.hooj0.fabric.sdk.commons.core.creator;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.Reader;
 import java.io.Serializable;
-import java.io.StringReader;
 import java.security.PrivateKey;
 
 import org.apache.commons.io.IOUtils;
-import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.openssl.PEMParser;
-import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.apache.commons.lang3.StringUtils;
 import org.hyperledger.fabric.sdk.Enrollment;
 
 import io.github.hooj0.fabric.sdk.commons.AbstractObject;
 import io.github.hooj0.fabric.sdk.commons.FabricCreatorException;
 import io.github.hooj0.fabric.sdk.commons.cache.FabricStoreCache;
 import io.github.hooj0.fabric.sdk.commons.domain.OrganizationUser;
+import io.github.hooj0.fabric.sdk.commons.util.PrivateKeyConvertUtils;
 
 /**
  * Organization User Creator
+ * @changelog add peer adimn cache store support
  * @author hoojo
  * @createDate 2018年7月22日 上午11:40:33
  * @file OrganizationUserCreatorImpl.java
@@ -33,9 +30,13 @@ import io.github.hooj0.fabric.sdk.commons.domain.OrganizationUser;
 public class OrganizationUserCreatorImpl extends AbstractObject implements OrganizationUserCreator {
 	
 	private FabricStoreCache<OrganizationUser> storeCache;
+	private FabricStoreCache<String> certStoreCache;
+	private FabricStoreCache<String> keyStoreCache;
 	
-	public OrganizationUserCreatorImpl(FabricStoreCache<OrganizationUser> storeCache) {
+	public OrganizationUserCreatorImpl(FabricStoreCache<OrganizationUser> storeCache, FabricStoreCache<String> certStoreCache, FabricStoreCache<String> keyStoreCache) {
 		this.storeCache = storeCache;
+		this.certStoreCache = certStoreCache;
+		this.keyStoreCache = keyStoreCache;
 	}
 	
 	@Override
@@ -50,16 +51,31 @@ public class OrganizationUserCreatorImpl extends AbstractObject implements Organ
 
 			user = new OrganizationUser(name, org, storeCache);
 
-			String certificate = new String(IOUtils.toByteArray(new FileInputStream(certificateFile)), "UTF-8");
-			PrivateKey privateKey = getPrivateKeyFromBytes(IOUtils.toByteArray(new FileInputStream(privateKeyFile)));
+			String[] key = new String[] { org, name };
+			String storeCert = certStoreCache.getStore(key), storeKey = keyStoreCache.getStore(key);
+			
+			Enrollment enrollment = null;
+			if (StringUtils.isBlank(storeCert) || StringUtils.isBlank(storeKey)) {
+				String certificate = new String(IOUtils.toByteArray(new FileInputStream(certificateFile)), "UTF-8");
+				byte[] keyBytes = IOUtils.toByteArray(new FileInputStream(privateKeyFile));
+				
+				PrivateKey privateKey = PrivateKeyConvertUtils.getPrivateKeyFromBytes(keyBytes);
+				enrollment = new UserEnrollement(privateKey, certificate);
+				
+				// 保存证书 key、cert
+				certStoreCache.setStore(key, certificate);
+				keyStoreCache.setStore(key, new String(keyBytes, "UTF-8"));
+			} else {
+				enrollment = new UserEnrollement(PrivateKeyConvertUtils.getPrivateKeyFromBytes(storeKey.getBytes("UTF-8")), storeCert);
+			}
 
 			user.setMspId(mspId);
 			// 登记认证
-			user.setEnrollment(new StoreEnrollement(privateKey, certificate));
+			user.setEnrollment(enrollment);
 			
 			return user;
 		} catch (Exception e) {
-			throw new FabricCreatorException(e, "Get '%s.%s' OrganizationUser Member from cache exception:", org, name);
+			throw new FabricCreatorException(e, "Get '%s.%s' OrganizationUser Member from cache exception: %s", org, name, e.getMessage());
 		}	
 	}
 	
@@ -76,33 +92,13 @@ public class OrganizationUserCreatorImpl extends AbstractObject implements Organ
 		return user;
 	}
 	
-	/**
-	 * 将byte字节私钥转换成 PrivateKey 对象
-	 * @author hoojo
-	 * @createDate 2018年6月13日 上午11:28:54
-	 * @param data key
-	 * @return PrivateKey
-	 */
-	private static PrivateKey getPrivateKeyFromBytes(byte[] data) throws Exception {
-		final Reader pemReader = new StringReader(new String(data));
-
-		final PrivateKeyInfo pemPair;
-		try (PEMParser pemParser = new PEMParser(pemReader)) {
-			pemPair = (PrivateKeyInfo) pemParser.readObject();
-		}
-
-		PrivateKey privateKey = new JcaPEMKeyConverter().setProvider(BouncyCastleProvider.PROVIDER_NAME).getPrivateKey(pemPair);
-
-		return privateKey;
-	}
-	
-	private static final class StoreEnrollement implements Enrollment, Serializable {
+	public static final class UserEnrollement implements Enrollment, Serializable {
 
 		private static final long serialVersionUID = -2784835212445309006L;
 		private final PrivateKey privateKey;
 		private final String certificate;
 
-		public StoreEnrollement(PrivateKey privateKey, String certificate) {
+		public UserEnrollement(PrivateKey privateKey, String certificate) {
 			this.certificate = certificate;
 			this.privateKey = privateKey;
 		}
