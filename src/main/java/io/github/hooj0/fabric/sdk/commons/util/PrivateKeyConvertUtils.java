f5d11a4e1843aea93fb59c579dc103238fc7619a
hoojo <hoojo@qq.com>
2018-08-09 19:38:29 +0800

    :sparkles: :bento: :tada:  Init add private key converter utils

---
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/util/PrivateKeyConvertUtils.java
@@ -0,0 +1,96 @@
+package io.github.hooj0.fabric.sdk.commons.util;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
+import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
+
+import sun.misc.BASE64Decoder;
+
+/**
+ * private key converter utils
+ * @author hoojo
+ * @createDate 2018年8月9日 下午4:42:39
+ * @file PrivateKeyConvertUtils.java
+ * @package io.github.hooj0.fabric.sdk.commons.util
+ * @project fabric-sdk-commons
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public abstract class PrivateKeyConvertUtils {
+
+	private static final Pattern compile = Pattern.compile("^-----BEGIN CERTIFICATE-----$" + "(.*?)" + "\n-----END CERTIFICATE-----\n", Pattern.DOTALL | Pattern.MULTILINE);
+	
+	/**
+	 * 将byte字节私钥转换成 PrivateKey 对象
+	 * @author hoojo
+	 * @createDate 2018年6月13日 上午11:28:54
+	 * @param data key
+	 * @return PrivateKey
+	 */
+	public static PrivateKey getPrivateKeyFromBytes(byte[] data) throws Exception {
+	
+		final Reader pemReader = new StringReader(new String(data));
+
+		final PrivateKeyInfo pemPair;
+		try (PEMParser pemParser = new PEMParser(pemReader)) {
+			pemPair = (PrivateKeyInfo) pemParser.readObject();
+		}
+
+		PrivateKey privateKey = new JcaPEMKeyConverter().setProvider(BouncyCastleProvider.PROVIDER_NAME).getPrivateKey(pemPair);
+
+		return privateKey;
+	}
+	
+	public static String getStringFromPrivateKey(PrivateKey privateKey) throws IOException {
+		StringWriter stringWriter = new StringWriter();
+
+		JcaPEMWriter writer = new JcaPEMWriter(stringWriter);
+		writer.writeObject(privateKey);
+		writer.close();
+		
+		return stringWriter.toString();
+	}
+	
+    /**
+     * 转换私钥
+     * @param base64 String to PrivateKey
+     * @throws Exception
+     */
+    public static PrivateKey getPrivateKey(String key) throws Exception {
+          byte[] keyBytes;
+          keyBytes = (new BASE64Decoder()).decodeBuffer(key);
+          PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+          KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+          PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+          return privateKey;
+    }
+	
+	public static String formatPEM(String pemFormat) {
+        String ret = pemFormat;
+
+        final Matcher matcher = compile.matcher(pemFormat);
+        if (matcher.matches()) {
+            final String base64Part = matcher.group(1).replaceAll("\n", "");
+            Base64.Decoder b64dec = Base64.getDecoder();
+            ret = new String(b64dec.decode(base64Part.getBytes(UTF_8)));
+        } 
+
+        return ret;
+    }
+}
