79f49ecf9e7032d7217a0f98c964bb72d3db7ac6
hoojo <hoojo@qq.com>
2018-08-10 13:52:11 +0800

    :sparkles: :bento: :recycle:  Changed Organization User store cache support

--- src/main/java/io/github/hooj0/fabric/sdk/commons/cache/support/OrganizationUserStoreCache.java
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/cache/support/OrganizationUserStoreCache.java
@@ -1,20 +1,10 @@
 package io.github.hooj0.fabric.sdk.commons.cache.support;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 
-import org.bouncycastle.util.encoders.Hex;
-
-import io.github.hooj0.fabric.sdk.commons.FabricCacheException;
-import io.github.hooj0.fabric.sdk.commons.cache.AbstractStoreCache;
 import io.github.hooj0.fabric.sdk.commons.cache.CacheKeyPrefix;
 import io.github.hooj0.fabric.sdk.commons.domain.OrganizationUser;
 import io.github.hooj0.fabric.sdk.commons.store.FabricKeyValueStore;
-import lombok.extern.slf4j.Slf4j;
 
 /**
  * Organization User store cache support
@@ -27,72 +17,17 @@ import lombok.extern.slf4j.Slf4j;
  * @email hoojo_@126.com
  * @version 1.0
  */
-@Slf4j
-public class OrganizationUserStoreCache extends AbstractStoreCache<OrganizationUser> {
+public class OrganizationUserStoreCache extends AbstractJavaObjectStoreCache<OrganizationUser> {
 	
 	public OrganizationUserStoreCache() {
-		super(CacheKeyPrefix.ORGANIZATION_USER_PREFIX);
+		super(CacheKeyPrefix.ORGANIZATION_USER_PREFIX, OrganizationUserStoreCache.class);
 	}
 	
 	public OrganizationUserStoreCache(File keyValueStoreFile) {
-		super(CacheKeyPrefix.ORGANIZATION_USER_PREFIX, keyValueStoreFile);
+		super(CacheKeyPrefix.ORGANIZATION_USER_PREFIX, keyValueStoreFile, OrganizationUserStoreCache.class);
 	}
 	
 	public OrganizationUserStoreCache(FabricKeyValueStore keyValueStore) {
-		super(CacheKeyPrefix.ORGANIZATION_USER_PREFIX, keyValueStore);
-	}
-
-	@Override
-	public String serialize(OrganizationUser store) {
-		ByteArrayOutputStream bos = null;
-		ObjectOutputStream oos = null;
-		
-		String serializeStrings = null;
-		try {
-			bos = new ByteArrayOutputStream();
-			oos = new ObjectOutputStream(bos);
-			
-			oos.writeObject(store);
-			oos.flush();
-			
-			serializeStrings = Hex.toHexString(bos.toByteArray());
-		} catch (Exception e) {
-			throw new FabricCacheException(e, "Organization User serialize exception");
-		} finally {
-			try {
-				if (oos != null) oos.close();
-				if (bos != null) bos.close();
-			} catch (Exception e) {
-				log.error(e.getMessage(), e);
-			}
-		}
-		
-		return serializeStrings;
-	}
-
-	@Override
-	public OrganizationUser deserialize(String value) {
-		
-		ByteArrayInputStream bis = null;
-		ObjectInputStream ois = null;
-		try {
-			// 用户被发现在关键值存储中，因此恢复状态
-			byte[] serialized = Hex.decode(value);
-
-			// 反序列化，解组对象
-			bis = new ByteArrayInputStream(serialized);
-			ois = new ObjectInputStream(bis);
-
-			return (OrganizationUser) ois.readObject();
-		} catch (Exception e) {
-			throw new FabricCacheException(e, "Organization User deserialize exception");
-		} finally {
-			try {
-				if (bis != null) bis.close();
-				if (bis != null) bis.close();
-			} catch (IOException e) {
-				log.error(e.getMessage(), e);
-			}
-		}
+		super(CacheKeyPrefix.ORGANIZATION_USER_PREFIX, keyValueStore, OrganizationUserStoreCache.class);
 	}
 }
