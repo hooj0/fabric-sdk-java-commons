947cb9a21746d402c0b306930bafc4fcb77b6288
hoojo <hoojo@qq.com>
2018-08-10 13:52:27 +0800

    :sparkles: :bento: :tada:  Init add hf ca enroll ment store cache support

---
+++ src/main/java/io/github/hooj0/fabric/sdk/commons/cache/support/HFCAEnrollmentStoreCache.java
@@ -0,0 +1,34 @@
+package io.github.hooj0.fabric.sdk.commons.cache.support;
+
+import java.io.File;
+
+import org.hyperledger.fabric_ca.sdk.HFCAEnrollment;
+
+import io.github.hooj0.fabric.sdk.commons.cache.CacheKeyPrefix;
+import io.github.hooj0.fabric.sdk.commons.store.FabricKeyValueStore;
+
+/**
+ * hf ca enroll ment store cache support
+ * @author hoojo
+ * @createDate 2018年8月10日 上午10:11:29
+ * @file HFCAEnrollmentStoreCache.java
+ * @package io.github.hooj0.fabric.sdk.commons.cache.support
+ * @project fabric-sdk-commons
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+public class HFCAEnrollmentStoreCache extends AbstractJavaObjectStoreCache<HFCAEnrollment> {
+
+	public HFCAEnrollmentStoreCache() {
+		super(CacheKeyPrefix.ENROLLEMENT_USER_PREFIX, OrganizationUserStoreCache.class);
+	}
+	
+	public HFCAEnrollmentStoreCache(File keyValueStoreFile) {
+		super(CacheKeyPrefix.ENROLLEMENT_USER_PREFIX, keyValueStoreFile, OrganizationUserStoreCache.class);
+	}
+	
+	public HFCAEnrollmentStoreCache(FabricKeyValueStore keyValueStore) {
+		super(CacheKeyPrefix.ENROLLEMENT_USER_PREFIX, keyValueStore, OrganizationUserStoreCache.class);
+	}
+}
